3da4cee6ab86701f74336575ef3eda893905584f
kameswarab <57407412+kameswarab@users.noreply.github.com>
2020-09-09 13:04:56 +0530

    Update HotelController.java
    dasdada

--- src/main/java/com/khoubyari/example/api/rest/HotelController.java
+++ src/main/java/com/khoubyari/example/api/rest/HotelController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
 
@@ -106,7 +107,7 @@ public class HotelController extends AbstractRestHandler {
     @ResponseStatus(HttpStatus.OK)
     public
     @ResponseBody
-    String testAPI( HttpServletRequest request, HttpServletResponse response,@RequestBody Map<String,String> commadMap) throws Exception {
+    String testAPI( HttpServletRequest request, HttpServletResponse response,@RequestBody Map<String,String> commadMap) throws IOException, InterruptedException  {
        
 		String commnd = commadMap.get("command");
 		log.info("commnd---- : "+commnd);
